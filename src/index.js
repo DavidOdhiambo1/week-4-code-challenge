c13443d5deefc25af1fd286fc5ed051a9e2180b4
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-18 22:09:10 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,55 @@
-// Your code here
+
+function initialize() {
+    const baseUrl = "http://localhost:3000"
+   
+// See the first movie's details
+    fetch ( `${baseUrl}/films/1`)
+    .then(res => res.json())
+    .then(movie => {
+        const poster = document.getElementById('poster')
+        poster.src = movie['poster']
+        const title = document.getElementById('title')
+        title.textContent = movie.title
+        const runtime = document.getElementById('runtime')
+        runtime.textContent = `Runtime: ${movie.runtime} minutes`
+        const description = document.getElementById('film-info')
+        description.textContent = movie.description
+        const showtime = document.getElementById('showtime')
+        showtime.textContent = movie.showtime
+        const availableTickets = document.getElementById('ticket-num')
+        availableTickets.textContent = movie.capacity - movie.tickets_sold
+
+        // buy a ticket
+    const button = document.querySelector('button')
+    button.addEventListener('click', ()=>{
+        if (availableTickets.textContent > 0) {
+            availableTickets.textContent -=1
+        } else {
+            availableTickets.textContent = "Sold out"
+        }
+
+    })
+    console.log(button)
+
+    })
+
+// list all movies
+    fetch(`${baseUrl}/films`)
+    .then(res => res.json())
+    .then(movies => {
+        movies.forEach( movie => {
+            const ul = document.getElementById('films')
+            const li = document.createElement('li')
+            li.classList = "film item"
+            li.textContent = movie.title
+            ul.appendChild(li)
+        })
+    })
+
+    
+}
+
+
+document.addEventListener("DOMContentLoaded", initialize
+)
+
